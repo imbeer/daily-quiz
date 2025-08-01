dd023e431077704696338c4dbcd3ef4a7d720174
imbeer <alekceykocenko@gmail.com>
2025-08-01 15:13:44 +0300

    change theme according to figma

--- app/src/main/java/com/toadthegod/dailyquiz/ui/theme/Color.kt
+++ app/src/main/java/com/toadthegod/dailyquiz/ui/theme/Color.kt
@@ -2,10 +2,12 @@ package com.toadthegod.dailyquiz.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val White = Color(0xFFFFFFFF)
+val SemiWhite = Color(0xFFF3F3F3)
+val Grey = Color(0xFFBABABA)
+val DarkLilac = Color(0xFF2B0063)
+val Lilac = Color(0xFF7067FF)
+val LightLilac = Color(0xFFBCB7FF)
+val Yellow = Color(0xFFFFB800)
+val Green = Color(0xFF00AE3A)
+val Red = Color(0xFFE70000)
--- app/src/main/java/com/toadthegod/dailyquiz/ui/theme/Theme.kt
+++ app/src/main/java/com/toadthegod/dailyquiz/ui/theme/Theme.kt
@@ -1,54 +1,37 @@
 package com.toadthegod.dailyquiz.ui.theme
 
-import android.os.Build
-import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.ColorScheme
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.graphics.Color
 
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+private val colorScheme = lightColorScheme(
+    primary = Lilac,
+    onPrimary = White,
+    secondary = SemiWhite,
+    onSecondary = Color.Black,
+    tertiary = Grey,
+    onTertiary = White,
+    surface = White,
+    onSurface = LightLilac,
+    background = Lilac,
+    outline = DarkLilac,
+    error = Red,
 )
 
-private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+val ColorScheme.success: Color
+    @Composable
+    get() = Green
 
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
-)
+val ColorScheme.rating: Color
+    @Composable
+    get() = Yellow
 
 @Composable
 fun DailyQuizTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
-    }
-
     MaterialTheme(
         colorScheme = colorScheme,
         typography = Typography,
