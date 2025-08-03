89849c03d5fe8ba0430bdb5d5871d633ba66d106
imbeer <alekceykocenko@gmail.com>
2025-08-03 19:26:07 +0300

    fix animations

--- app/src/main/java/com/toadthegod/dailyquiz/ui/ScreenTransitions.kt
+++ app/src/main/java/com/toadthegod/dailyquiz/ui/ScreenTransitions.kt
@@ -1,14 +1,28 @@
 package com.toadthegod.dailyquiz.ui
 
 
+import androidx.compose.animation.AnimatedContentTransitionScope
 import androidx.compose.animation.EnterTransition
+import androidx.compose.animation.ExitTransition
+import androidx.compose.animation.core.tween
 import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.navigation.NavBackStackEntry
+import com.ramcosta.composedestinations.scope.DestinationScope
 import com.ramcosta.composedestinations.spec.DestinationStyle
 import androidx.compose.animation.AnimatedContentTransitionScope as AnimScope
 
 object ScreenTransitions : DestinationStyle.Animated() {
-    override val enterTransition: (AnimScope<NavBackStackEntry>.() -> EnterTransition?) = {
-        fadeIn()
+    private const val DURATION = 300
+
+    override val enterTransition: AnimatedContentTransitionScope<NavBackStackEntry>.() -> EnterTransition? = {
+        fadeIn(animationSpec = tween(DURATION))
     }
+
+    override val exitTransition: AnimatedContentTransitionScope<NavBackStackEntry>.() -> ExitTransition? = {
+        fadeOut(animationSpec = tween(DURATION))
+    }
+
+    override val popEnterTransition = enterTransition
+    override val popExitTransition = exitTransition
 }
